a80f3023b2db6cae972285c30d6eb805645cc650
OSTorres2003 <90116649+OSTorres2003@users.noreply.github.com>
2025-12-01 15:13:00 -0600

    ss

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from './users/users.module';
 @Module({
 imports: [
     // --- Configuración de la Conexión a PostgreSQL ---
