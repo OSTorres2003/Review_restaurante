42741277a1ab9474f08f6840ad22d0522a941b62
OSTorres2003 <90116649+OSTorres2003@users.noreply.github.com>
2025-12-02 14:44:37 -0600

    conexion a base de datos y pruebas de conexion

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,13 +8,10 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    // Cargar variables de entorno desde .env
     ConfigModule.forRoot({
       isGlobal: true,
-      // envFilePath: '.env', // opcional, por defecto ya usa .env en la raíz
     }),
 
-    // Conexión a PostgreSQL (Supabase)
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST as string,
@@ -22,11 +19,8 @@ import { AuthModule } from './auth/auth.module';
       username: process.env.DB_USER as string,
       password: process.env.DB_PASSWORD as string,
       database: process.env.DB_NAME as string,
-
       autoLoadEntities: true,
-      synchronize: true, // SOLO en desarrollo
-
-      // Necesario para Supabase (equivalente a "Require" en DataGrip)
+      synchronize: false, // muy importante con Supabase
       ssl: {
         rejectUnauthorized: false,
       },
